d647e755557ca1fc1e8462818c79ef3528ca32b7
leodotng <josh@leo.ng>
2018-04-12 14:07:09 -0600

    changed title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Josh Leong - http://josh.leo.ng. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      &copy; Josh Leong - http://josh.leo.ng. Built with:{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+    </p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: React.PropTypes.bool
+  timeout: React.PropTypes.bool,
 }
 
 export default Footer
