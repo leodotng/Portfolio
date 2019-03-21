9999287cab2c63b693af1db412ed8bc4c517c1be
Josh Leong <josh@leo.ng>
2019-03-20 23:18:32 -0600

    updated resume file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Dimension by HTML5 UP"
+    title: "Portfolio Josh Leong Software Developer",
+    author: "Josh Leong",
+    description: "Josh Leong Portfolio"
   },
   pathPrefix: '/',
   plugins: [
